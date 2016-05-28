3b3360684315d0abb7c62bcc305ba6867d0a123b
Benjamin Johnson <bkj986@uowmail.edu.au>
2016-05-28 16:56:33 +1000

    Initial Commit

---
+++ ArtGallery/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArtGallery
+//
+//  Created by Benjamin Johnson on 28/05/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
